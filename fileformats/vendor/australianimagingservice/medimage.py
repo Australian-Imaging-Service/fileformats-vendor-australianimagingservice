408e9dd626f755fc8bdd0511ad3a9d937df7f3b3
Thomas G. Close <tom.g.close@gmail.com>
2026-09-23 17:43:55 +1000

    touched up dicom sample ext

--- fileformats/vendor/australianimagingservice/medimage.py
+++ fileformats/vendor/australianimagingservice/medimage.py
@@ -8,6 +8,7 @@ class XnatSnapshot(MedicalImage, Gif):
 
 
 class DicomSample(MedicalImage, Dicom):
-    """An extracted DICOM slice to be stored alongside a DICOM zip archive so its metadata can be extracted"""
+    """An extracted DICOM slice to be stored alongside a DICOM zip
+    archive so its metadata can be extracted"""
 
-    ext = ""
+    ext = None
