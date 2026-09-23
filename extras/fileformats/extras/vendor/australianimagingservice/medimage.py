86630d16b2f96a5cfbada1388aa17c83e491204a
Thomas G. Close <tom.g.close@gmail.com>
2026-09-23 17:21:13 +1000

    added converters from dicom zip

--- extras/fileformats/extras/vendor/australianimagingservice/medimage.py
+++ extras/fileformats/extras/vendor/australianimagingservice/medimage.py
@@ -1,3 +1,65 @@
 # Implement 'extras' for the formats defined in
 # fileformats.vendor.australianimagingservice.medimage here, see
 # https://arcanaframework.github.io/fileformats/developer/extras.html
+
+from fileformats.core import converter
+from fileformats.application import Zip
+from fileformats.medimage import DicomSeries
+from pydra.compose import python
+
+from fileformats.vendor.australianimagingservice.medimage import (
+    DicomSample,
+    XnatSnapshot,
+)
+
+
+@converter
+@python.define(outputs=["out_file"])  # type: ignore[untyped-decorator]def SnapshotDicomCollection(
+def SnapshotZippedDicomCollection(
+    in_file: Zip[DicomSeries],
+) -> XnatSnapshot:
+    """Snapshots of a DICOM collection for display in XNAT's UI
+
+    Parameters
+    ----------
+    in_file : Zip[DicomSeries]
+        the input zipped DICOM series to create snapshots from
+
+    Returns
+    -------
+    out_file: XnatSnapshot
+        the resulting XNAT snapshot of the DICOM collection
+
+    Raises
+    ------
+    ValueError
+        when mutually exclusive "extract_volume" and "to_4d" options are provided
+    """
+    raise NotImplementedError(
+        "SnapshotDicomCollection converter is not yet implemented"
+    )
+
+
+@converter
+@python.define(outputs=["out_file"])  # type: ignore[untyped-decorator]def SnapshotDicomCollection(
+def SampleZippedDicomCollection(
+    in_file: Zip[DicomSeries],
+) -> DicomSample:
+    """Samples a DICOM collection from a zipped DICOM collection
+
+    Parameters
+    ----------
+    in_file : Zip[DicomSeries]
+        the input zipped DICOM series to create a sample from
+
+    Returns
+    -------
+    out_file: DicomSample
+        the resulting sample of the DICOM collection
+
+    Raises
+    ------
+    ValueError
+        when mutually exclusive "extract_volume" and "to_4d" options are provided
+    """
+    raise NotImplementedError("SampleDicomCollection converter is not yet implemented")
